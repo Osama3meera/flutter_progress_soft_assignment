01ea7d32f79fd7543e91bcc5669a4e70b300dedc
Osama3meera <osamahasan3meera@gmail.com>
2024-07-14 21:07:32 +0300

    initial commit - added presentation package for ui

---
+++ android/app/src/main/java/com/example/osamahasanprogresssoft/osama_hasan_progress_soft/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.osamahasanprogresssoft.osama_hasan_progress_soft;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
